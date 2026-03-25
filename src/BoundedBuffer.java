4131d4447919b5df9f9e9d07e7aea8d36f6584c7
CSU-Ryan <enderbro@colostate.edu>
2026-03-24 20:07:53 -0600

    Implements BoundedBuffer correctly

--- src/BoundedBuffer.java
+++ src/BoundedBuffer.java
@@ -1,25 +1,51 @@
 public class BoundedBuffer {
-    final Double[] array = new Double[1000];
-    int printTo = 0;
-    int readFrom = -1;
+    final int BUFFER_LENGTH = 1000;
+    final double[] array = new double[BUFFER_LENGTH];
 
-    // THIS IS DEFINITELY WRONG
+    int writeTo = 0;
+    int readFrom = 0;
+    int queueLength = 0;
 
-    public synchronized void print(Double data) throws InterruptedException {
-        while (printTo == readFrom) {
-            wait();
-        }
 
-        array[printTo] = data;
-        printTo++;
-        if (readFrom == -1) readFrom++;
+    /// Used to update indices, reflecting added queue item.
+    private synchronized void incrementQueue() {
+        System.out.println("Buffer: Incrementing queue.");
+        writeTo = (writeTo + 1) % BUFFER_LENGTH;
+        queueLength++;
+        notify();
     }
 
-    public synchronized Double read() throws InterruptedException {
-        while (readFrom == printTo) {
+    /// Used to update indices, reflecting removed queue item.
+    private synchronized void decrementQueue() {
+        System.out.println("Buffer: decrementing queue.");
+        readFrom = (readFrom + 1) % BUFFER_LENGTH;
+        queueLength--;
+        notify();
+    }
+
+
+    public void write(double data) throws InterruptedException {
+        System.out.printf("Buffer write: checking queue length (%d)\n", queueLength);
+        while (queueLength >= BUFFER_LENGTH) {
+            System.out.println("Buffer write: Waiting!");
             wait();
         }
 
-        return array[readFrom++];
+        array[writeTo] = data;
+        System.out.printf("Buffer write: Wrote %f to %d.\n", data, writeTo);
+        incrementQueue();
+    }
+
+    public double read() throws InterruptedException {
+        System.out.printf("Buffer read: checking queue length (%d)\n", queueLength);
+        while (queueLength <= 0) {
+            System.out.println("Buffer read: Waiting!");
+            wait();
+        }
+
+        double value = array[readFrom];
+        System.out.printf("Buffer read: Read %f from %d.\n", value, readFrom);
+        decrementQueue();
+        return value;
     }
 }
