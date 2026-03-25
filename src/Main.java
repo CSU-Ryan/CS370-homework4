d0d55d6da06b37b679163092a8bc3ea576ee1fdf
CSU-Ryan <enderbro@colostate.edu>
2026-03-24 20:58:44 -0600

    Implements Main

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,24 @@
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
-    public static void main(String[] args) {
 
+    public static void main(String[] args) {
+        BoundedBuffer buffer = new BoundedBuffer();
+        Producer producer = new Producer(buffer);
+        Consumer consumer = new Consumer(buffer);
+
+        Thread producerThread = new Thread(producer);
+        Thread consumerThread = new Thread(consumer);
+
+        producerThread.start();
+        consumerThread.start();
+
+        try {
+            producerThread.join();
+            consumerThread.join();
+        } catch (InterruptedException e) {
+            System.err.println("ERROR: thread was interrupted.");
+            throw new RuntimeException(e);
+        }
     }
 }
